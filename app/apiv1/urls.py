d30be09d7ffeb2189690f3b63620622b2218f17f
Miguel Concepcion <miguel.concepcion@outlook.com>
2017-04-28 18:40:33 +0800

    change url definition to router

--- app/apiv1/urls.py
+++ app/apiv1/urls.py
@@ -1,27 +1,8 @@
-from rest_framework.urlpatterns import format_suffix_patterns
-from django.conf.urls import url, include
+from rest_framework.routers import DefaultRouter
 from apiv1.views import user as user_view
 
-user_patterns = format_suffix_patterns([
-    url(r'^signup/$', user_view.UserCreateViewSet.as_view({'post':'create'}), name='user_signup'),
-    url(
-        r'^activate/(?P<pk>\d+)/$',
-        user_view.UserCreateViewSet.as_view({'get':'activate'}),
-        name='user_activate'
-    ),
-    url(r'^list/$', user_view.UserViewSet.as_view({'get':'list'}), name='user_list'),
-    url(
-        r'^change-password/$',
-        user_view.UserViewSet.as_view({'post': 'change_password'}),
-        name='user_changepass'
-    ),
-    url(
-        r'^login/$',
-        user_view.UserLoginViewSet.as_view({'post': 'login'}),
-        name='user_login'
-    ),
-])
+router = DefaultRouter()
+router.register(r'users', user_view.UserViewSet)
 
-urlpatterns = [
-    url(r'^user/', include(user_patterns, namespace='user')),
-]
+
+urlpatterns = router.urls
