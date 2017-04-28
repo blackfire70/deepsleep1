9c7ccf066c07e305a143511982fba6511d765253
Miguel Concepcion <miguel.concepcion@outlook.com>
2017-04-28 17:21:21 +0800

    flake8 compliance

--- app/apiv1/serializers.py
+++ app/apiv1/serializers.py
@@ -6,14 +6,26 @@ from rest_framework.validators import UniqueValidator
 
 class UserCreateSerializer(serializers.ModelSerializer):
 
-    password = serializers.CharField(max_length=200, write_only=True, required=True)
+    password = serializers.CharField(
+        max_length=200,
+        write_only=True,
+        required=True
+    )
     email = serializers.EmailField(
         max_length=30,
         required=True,
         validators=[UniqueValidator(queryset=get_user_model().objects.all())]
     )
-    first_name = serializers.CharField(max_length=20, required=False, default='')
-    last_name = serializers.CharField(max_length=20, required=False, default='')
+    first_name = serializers.CharField(
+        max_length=20,
+        required=False,
+        default=''
+    )
+    last_name = serializers.CharField(
+        max_length=20,
+        required=False,
+        default=''
+    )
     is_active = serializers.BooleanField(read_only=True)
 
     class Meta:
@@ -48,7 +60,7 @@ class UserLoginSerializer(serializers.ModelSerializer):
         max_length=30,
         required=True
     )
-    password = serializers.CharField(max_length=200,required=True)
+    password = serializers.CharField(max_length=200, required=True)
 
     class Meta:
         model = get_user_model()
